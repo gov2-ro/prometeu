09a8b527fa7e4e3ae6a786455f3721de16c650a1
pax <pax@mioritics.ro>
2023-08-30 01:31:49 +0300

    try them roads once more

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # gov2.ro Git Scraping
 
 [gitscraping](https://simonwillison.net/tags/gitscraping/)  [Git scraping: track changes over time by scraping to a Git repository](https://simonwillison.net/2020/Oct/9/git-scraping/) 
+
+
+see also: [scrape-roads-dot-ca-gov](https://github.com/simonw/scrape-roads-dot-ca-gov) 
--- cmteb.py
+++ cmteb.py
@@ -1,27 +1,67 @@
-from disaster_scrapers import Scraper
-import requests
-import json, re
+import os, json, re, requests,  shutil, datetime
+import wget
+from bs4 import BeautifulSoup
 
+ziurl = 'https://www.cmteb.ro/harta_stare_sistem_termoficare_bucuresti.php'
+data_root = 'data/cmteb/'
 
-class cmteb(Scraper):
-    owner = "gov2-ro"
-    repo = "github-scrapers"
-    # repo = "gh-scraping-data"
-    # filepath = "misc/cmteb.json"
-    filepath = "cmteb.json"
+# TODO: prepare for github scraping
 
-    url = 'https://www.cmteb.ro/harta_stare_sistem_termoficare_bucuresti.php'
+def fetch_and_save_html(url):
+    current_date_time = datetime.datetime.now().strftime('%y-%m-%d %H:%M')
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(f"Failed to fetch data from the website. HTTP Error {response.status_code}")
+        return None
+
+    # html_file = f'{data_root}/cached/{current_date_time}.html'
+    # with open(html_file, 'w', encoding='utf-8') as file:
+    #     file.write(response.text)
+
+    return response.text
+
+def extract_json_from_html(html_content):
+
+    passed_features = {}
+    for color in ['verde', 'rosu', 'galben']:
+        pattern = r'var passedFeatures_{} = (.+?);'.format(color)
+        data = re.search(pattern, html_content)
+        if data:
+            data_str = data.group(1)
+            passed_features[color] = json.loads(data_str)
+        # else:
+            # print(f"Data for '{color}' not found in the HTML.")
+
+    return passed_features
+
+def save_json_to_file(data):
+    # current_date_time = datetime.datetime.now().strftime('%y-%m-%d %H:%M')
     
-    def fetch_data(self):
-        html_content = requests.get(self.url, timeout=10).text
-        passed_features = {}
-        # TODO: also list by location and note status color
-        for color in ['verde', 'rosu', 'galben']:
-            pattern = r'var passedFeatures_{} = (.+?);'.format(color)
-            data = re.search(pattern, html_content)
-            if data:
-                data_str = data.group(1)
-                passed_features[color] = json.loads(data_str)
+    json_file = f'{data_root}/latest.json'
+    with open(json_file, 'w') as file:
+        json.dump(data, file)
+
+    return data
+
+
+if __name__ == "__main__":
+    url = ziurl
+
+    cached_html = fetch_and_save_html(url)
+
+    data = extract_json_from_html(cached_html)
+
+    print(data)
+
+    save_json_to_file(data)
+
     
-        # return json.loads(content)
-        return passed_features
+
+    # moved_html_file = f'{data_root}/cached/parsed/{os.path.basename(cached_html_file)}'
+    # move_files(cached_html_file, moved_html_file)
+
+    # print(f"-- JSON data saved to '{json_file}' and HTML file moved to '{moved_html_file}' successfully.")
