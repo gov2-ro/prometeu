d3ffba248a442d5d011bc698a36c26a098718eac
pax <pax@mioritics.ro>
2023-09-04 23:58:25 +0300

    pause posturi buba

--- posturi-fetch-index-csv.py
+++ posturi-fetch-index-csv.py
@@ -1,33 +1,69 @@
 
 dataRoot = 'data/posturi/'
 csvFile = dataRoot + 'posturi.csv'
+from datetime import datetime
 import requests
 import csv
 from bs4 import BeautifulSoup
 import os
 
+
+def pdate2date(input_date):
+    romanian_months = {
+        'ianuarie': 'January',
+        'februarie': 'February',
+        'martie': 'March',
+        'aprilie': 'April',
+        'mai': 'May',
+        'iunie': 'June',
+        'iulie': 'July',
+        'august': 'August',
+        'septembrie': 'September',
+        'octombrie': 'October',
+        'noiembrie': 'November',
+        'decembrie': 'December'
+    }
+
+    # Convert the input date to a datetime object using the custom month dictionary
+    for romanian_month, english_month in romanian_months.items():
+        input_date = input_date.replace(romanian_month, english_month)
+
+    date_obj = datetime.strptime(input_date, "%d %B,%Y")
+
+    # Format the date as "dd/MM/yyyy"
+    formatted_date = date_obj.strftime("%d/%m/%Y")
+
+    # Print the formatted date
+    return formatted_date
+
+
 # Function to create the CSV file if it doesn't exist
 def create_csv_file():
     if not os.path.isfile(csvFile):
         with open(csvFile, "w", newline="", encoding="utf-8") as csv_file:
-            fieldnames = ['title', 'link_url', 'angajator', 'n', 'publicat', 'expira', 'locatie_name', 'locatie_url', 'dosar']
+            fieldnames = ['title', 'link_url', 'angajator', 'tip', 'publicat', 'expira', 'locatie_name', 'locatie_url', 'dosar']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
 
-# Function to load the latest 30 rows from the CSV file
+# Function to load the latest 10 rows from the CSV file
 def load_latest_data():
     try:
         latest_data = []
         with open(csvFile, "r", newline="", encoding="utf-8") as csv_file:
             reader = csv.DictReader(csv_file)
             rows = list(reader)
-            if len(rows) > 30:
-                latest_data = rows[-30:]  # Load the latest 30 rows
+            if len(rows) > 10:
+                latest_data = rows[-10:]  # Load the latest 10 rows
             else:
                 latest_data = rows
     except FileNotFoundError:
         latest_data = []
-    return latest_data
+    latest_list = []
+    
+    for rrow in latest_data:
+        latest_list.append(rrow['link_url']+rrow['publicat'])
+    print(latest_list)
+    return latest_list
 
 # Function to scrape data from a single page and write new data to a CSV file
 def scrape_and_save_page(url, page_number, existing_data):
@@ -42,24 +78,30 @@ def scrape_and_save_page(url, page_number, existing_data):
     for box in article_boxes:
         job = {}
         job['title'] = box.select_one('div.title a').text
-        job['link_url'] = box.select_one('div.title a')['href']
+        job['link_url'] = box.select_one('div.title a')['href'].replace('http://posturi.gov.ro/anunt/','')
+
         job['angajator'] = box.select_one('div.angajator').text
         job['tip'] = ', '.join([n.text for n in box.select('div.n')])
         
         data_div = box.select_one('li.data div.data')
-        
-        # Filter the elements by their text content
         data_items = [item.strip() for item in data_div.stripped_strings]
-        job['publicat'] = data_items[0].replace('Publicat în: ', '').strip()
+        # Filter the elements by their text content
+        data_publicat = data_items[0].replace('Publicat în: ', '').strip()       
+
+        job['publicat'] = pdate2date(data_publicat)
+
+        
         job['expira'] = data_items[1].replace('Expiră in ', '').strip()
         
         locatie_div = box.select_one('li.locatie div.locatie')
         job['locatie_name'] = locatie_div.text.strip()
-        job['locatie_url'] = locatie_div.a['href']
+        job['locatie_url'] = locatie_div.a['href'].replace('http://posturi.gov.ro/?judet=', '')
         
         job['dosar'] = box.select_one('div.dosar').text
         
         job_list.append(job)
+    
+        # check if exists
     
     # Combine the new data with existing data
     all_jobs = existing_data + job_list
@@ -67,7 +109,7 @@ def scrape_and_save_page(url, page_number, existing_data):
     # Check for new data by comparing with the latest 30 rows
     new_jobs = []
     for job in job_list:
-        if not any(job['link_url'] + job['locatie_name'] in row['link_url'] + row['locatie_name'] for row in existing_data):
+        if not any(job['link_url'] + job['publicat'] in row['link_url'] + row['publicat'] for row in existing_data):
             new_jobs.append(job)
     
     # Append new data to the CSV file
---
+++ posturi-fetch-index-csv2.py
@@ -0,0 +1,96 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+def load_existing_data():
+    try:
+        existing_data = []
+        with open("posturi_gov_ro.csv", "r", newline="", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file)
+            existing_data = list(reader)
+        return existing_data
+    except FileNotFoundError:
+        return []
+
+# Function to load the latest jobs from the CSV file
+def load_latest_jobs():
+    try:
+        latest_jobs = []
+        with open("posturi_gov_ro.csv", "r", newline="", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                if 'link_url' in row and 'publicat' in row:
+                    latest_jobs.append(f"{row['link_url']}+{row['publicat']}")
+        return latest_jobs
+    except FileNotFoundError:
+        return []
+
+
+# Function to scrape data from a single page and append it to a CSV file
+def scrape_and_save_page(url, page_number, existing_data, latest_jobs):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    job_list = []
+    
+    # Find all the article boxes
+    article_boxes = soup.select('article.box')
+    
+    for box in article_boxes:
+        job = {}
+        job['title'] = box.select_one('div.title a').text
+        job['link_url'] = box.select_one('div.title a')['href']
+        job['angajator'] = box.select_one('div.angajator').text
+        job['n'] = ', '.join([n.text for n in box.select('div.n')])
+        
+        data_div = box.select_one('li.data div.data')
+        
+        # Filter the elements by their text content
+        data_items = [item.strip() for item in data_div.stripped_strings]
+        job['publicat'] = data_items[0]
+        job['expira'] = data_items[1]
+        
+        locatie_div = box.select_one('li.locatie div.locatie')
+        job['locatie_name'] = locatie_div.text.strip()
+        job['locatie_url'] = locatie_div.a['href']
+        
+        job['dosar'] = box.select_one('div.dosar').text
+        
+        job_key = f"{job['link_url']}+{job['publicat']}"
+        
+        # Check if the job already exists in the latest_jobs list
+        if job_key in latest_jobs:
+            print(f"Job already exists: {job_key}. Stopping the script.")
+            return
+        
+        job_list.append(job)
+    
+    # Combine the new data with existing data
+    all_jobs = existing_data + job_list
+    
+    # Append all data to the same CSV file
+    fieldnames = ['title', 'link_url', 'angajator', 'n', 'publicat', 'expira', 'locatie_name', 'locatie_url', 'dosar']
+    with open("posturi_gov_ro.csv", "a", newline="", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        for job in job_list:
+            writer.writerow(job)
+    
+    return job_list
+
+# Function to scrape and paginate through all pages
+def scrape_all_pages(base_url, max_pages):
+    existing_data = load_existing_data()
+    latest_jobs = load_latest_jobs()
+    
+    for page_number in range(1, max_pages + 1):
+        url = f"{base_url}/page/{page_number}/"
+        print(f"Scraping page {page_number}...")
+        jobs = scrape_and_save_page(url, page_number, existing_data, latest_jobs)
+    
+    print("Scraping complete. Data appended incrementally to the same CSV file.")
+
+if __name__ == "__main__":
+    base_url = "http://posturi.gov.ro"
+    max_pages = 47
+    
+    scrape_all_pages(base_url, max_pages)
