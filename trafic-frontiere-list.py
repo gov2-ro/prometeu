ebfd6bca591016aad1402b834d980c12da9f7339
pax <pax@mioritics.ro>
2023-09-10 16:12:18 +0300

    pol frontiera - markers - toate punctele de frontieră

--- trafic-frontiere-list.py
+++ trafic-frontiere-list.py
@@ -1,26 +1,82 @@
+""" 
+Intrare: 5 artere; Iesire: 7 artere. Trafic intens - numar mare de autoturisme prezente la verificarile de frontiera pe sensul de iesire din tara. RECOMANDARE: pentru evitarea aglomerarilor, puteti folosi si alte puncte de trecere a frontierei!
+Denumire  |  Timp  |  Info  |  Unelte
+- [x] fetch tables/list
+- [ ] fetch map
+- [x] export CSV
+- [ ] Detect fields inside Info:
+    - [ ] nr artere intrare / ieșire
+    - [ ] recomandari: 
+    - [ ] trafic intens:
+    - [ ] tonaj admis
+    - [ ] orar
+- [x] export json
+- [ ] export GeoJson - see trafic-frontiere-markers.py
+ """
+
 
 url = 'https://www.politiadefrontiera.ro/ro/traficonline/'
-csv_filename = 'data/politia-de-frontiera/trafic-frontiere.csv'
+filename  = 'data/politia-de-frontiera/trafic-frontiere'
 
-import requests
+import requests, csv, re
+import pandas as pd
 from bs4 import BeautifulSoup
-import csv
-import re
-
+zicolumns = ['Denumire', 'Timp', 'Info', 'Latitude', 'Longitude', 'Status', 'Tip vehicul', 'Sens']
 # Define the base URL
 base_url = url
 
 # Define the sources with different url_vars
 sources = [
-    {"tip_vehicol": "Autoturisme", "sens": "Intrare", "url_vars": "?vt=1&vw=2&dt=1"},
-    {"tip_vehicol": "Autoturisme", "sens": "Ieșire", "url_vars": "?vt=1&vw=2&dt=2"},
-    {"tip_vehicol": "Camioane", "sens": "Intrare", "url_vars": "?vt=2&vw=2&dt=1"},
-    {"tip_vehicol": "Camioane", "sens": "Ieșire", "url_vars": "?vt=2&vw=2&dt=2"}
+    {"tip_vehicul": "Autoturisme", "sens": "Intrare", "url_vars": "?vw=2&vt=1&dt=1"},
+    {"tip_vehicul": "Autoturisme", "sens": "Ieșire", "url_vars": "?vw=2&vt=1&dt=2"},
+    {"tip_vehicul": "Camioane", "sens": "Intrare", "url_vars": "?vw=2&vt=2&dt=1"},
+    {"tip_vehicul": "Camioane", "sens": "Ieșire", "url_vars": "?vw=2&vt=2&dt=2"}
 ]
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-combined_data = []
+
+
+def extract_fields(html):
+    combined_data = []
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('div', {'id': 'maplist'}).find('table')
+
+    for row in table.find('tbody').find_all('tr'):
+        columns = row.find_all('td')
+        denumire = columns[0].text.strip()
+
+        timp_text = columns[1].text.strip()
+        timp_match = re.search(r'(\d+) min\.', timp_text)
+        timp = int(timp_match.group(1)) if timp_match else None
+
+        info = columns[2].text.strip()
+        # TODO: detect details in info, recomandări etc
+        """ 
+        Intrare: 5 artere; Iesire: 6 artere. Trafic intens - numar mare de autoturisme prezente la verificarile de frontiera pe ambele sensuri. RECOMANDARE: pentru evitarea aglomerarilor, puteti folosi si alte puncte de trecere a frontierei! 
+        - split by '.' into sentences
+        check for: recomandare, trafic intens -, arter
+            """
+        infoz=info.split('.')
+
+        unelte_url = columns[3].find('a')['href'] if columns[3].find('a') else ''
+        lat_long_match = re.search(r'(\d+\.\d+),(\d+\.\d+)', unelte_url)
+        if lat_long_match:
+            latitude = lat_long_match.group(1)
+            longitude = lat_long_match.group(2)
+        else:
+            latitude = ''
+            longitude = ''
+
+        timp_span = columns[1].find('span', {'class': re.compile(r'iwrow iws iw_\w+')})
+        color_match = re.search(r'iw_(\w+)', timp_span['class'][2]) if timp_span else None
+        level = color_match.group(1) if color_match else None
+
+        tip_vehicul = source['tip_vehicul']
+        sens = source['sens']
+
+        combined_data.append([denumire, timp, info, latitude, longitude, level, tip_vehicul, sens])
+    return combined_data
 
 for source in sources:
     url = base_url + source['url_vars']
@@ -28,45 +84,22 @@ for source in sources:
         response = requests.get(url, headers=headers)
     except requests.exceptions.ConnectionError:
         response.status_code = "Connection refused"
+        # TODO: logging
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find('div', {'id': 'maplist'}).find('table')
-
-        for row in table.find('tbody').find_all('tr'):
-            columns = row.find_all('td')
-            denumire = columns[0].text.strip()
-
-            timp_text = columns[1].text.strip()
-            timp_match = re.search(r'(\d+) min\.', timp_text)
-            timp = int(timp_match.group(1)) if timp_match else None
-
-            info = columns[2].text.strip()
-
-            unelte_url = columns[3].find('a')['href'] if columns[3].find('a') else ''
-            lat_long_match = re.search(r'(\d+\.\d+),(\d+\.\d+)', unelte_url)
-            if lat_long_match:
-                latitude = lat_long_match.group(1)
-                longitude = lat_long_match.group(2)
-            else:
-                latitude = ''
-                longitude = ''
-
-            timp_span = columns[1].find('span', {'class': re.compile(r'iwrow iws iw_\w+')})
-            color_match = re.search(r'iw_(\w+)', timp_span['class'][2]) if timp_span else None
-            level = color_match.group(1) if color_match else None
-
-            tip_vehicol = source['tip_vehicol']
-            sens = source['sens']
-
-            combined_data.append((denumire, timp, info, latitude, longitude, level, tip_vehicol, sens))
+        combined_data = extract_fields(response.text)
     else:
         print(f'Failed to fetch the URL for source: {source}. Status code: {response.status_code}')
+        # TODO: logging
 
 
-with open(csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
+with open(filename + '.csv', 'w', newline='', encoding='utf-8') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(['Denumire', 'Timp', 'Info', 'Latitude', 'Longitude', 'Level', 'tip_vehicol', 'sens'])
+    writer.writerow(zicolumns)
     writer.writerows(combined_data)
 
-print(f'Scraped & saved to {csv_filename}')
+csv_obj = pd.DataFrame(combined_data, columns = zicolumns)
+# csv_obj.to_json(filename + '.json', orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+csv_obj.to_json(filename + '.json', orient = "records", force_ascii = False, indent=2 )
+
+print(f'Scraped & saved to {filename}.csv/json')
---
+++ trafic-frontiere-markers.py
@@ -0,0 +1,124 @@
+""" 
+ 
+ """
+
+url = 'https://www.politiadefrontiera.ro/ro/traficonline/'
+filename = 'data/politia-de-frontiera/trafic-frontiere-map'
+
+import requests, csv, re, json5, json
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# import json
+
+# from utils.common import fetch_data, save_json_to_file, data_root
+
+
+zicolumns = ['Denumire', 'Timp', 'Info', 'Latitude', 'Longitude', 'Status', 'Tip vehicul', 'Sens']
+
+base_url = url
+sources = [{
+    "tip_vehicul": "Autoturisme",
+    "sens": "Intrare",
+        "url_vars": "?vw=1&vt=1&dt=1"
+    }, {
+        "tip_vehicul": "Autoturisme",
+        "sens": "Ieșire",
+        "url_vars": "?vw=1&vt=1&dt=2"
+    }, {
+        "tip_vehicul": "Camioane",
+        "sens": "Intrare",
+        "url_vars": "?vw=1&vt=2&dt=1"
+    }, {
+        "tip_vehicul": "Camioane",
+        "sens": "Ieșire",
+        "url_vars": "?vw=1&vt=2&dt=2"
+    }]
+
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+title_pattern = r'<strong>(.*?)<\/strong>'
+wait_time_pattern = r'Timp de așteptare (\d+) min.'
+info_pattern = r'hlrow">(.*?)<\/span>'
+vehicle_pattern = r'fa-(.*?)"></i>'
+status_pattern = r'iw_(.*?)">'
+
+def extract_json(carne):
+    soup = BeautifulSoup(carne, 'html.parser')
+    # script_tag = soup.find('script', text=re.compile(r'var markers = \[.*?\];', re.DOTALL))
+    script_tag = soup.find('script', string=re.compile(r'var markers = \[.*?\];', re.DOTALL))
+
+    javascript_code = script_tag.text
+
+    matches = re.search(r'var markers = (\[.*?\]);', javascript_code, re.DOTALL)
+    if matches:
+        json_str = matches.group(1)
+        json_str = json_str.replace('\n', '').replace('\t', '')      
+        markers = json5.loads(json_str)
+        return markers
+    else:
+        print("JSON data not found in the script tag.")
+
+combined_data = []
+
+for source in sources:
+    url = base_url + source['url_vars']
+    try:
+        response = requests.get(url, headers=headers)
+    except requests.exceptions.ConnectionError:
+        response.status_code = "Connection refused"
+        # TODO: logging
+
+    if response.status_code == 200:
+        jlist = extract_json(response.text)
+        # jjson is a list of json dicts
+        # detect fields in description
+        
+        for element in jlist:
+            newobj = {}
+            # title = wait_time = info = vehicle = status = ''
+            title = re.search(title_pattern, element['description']).group(1)
+            wait_time = int(re.search(wait_time_pattern, element['description']).group(1))
+            info = re.search(info_pattern, element['description']).group(1)
+            vehicle = re.search(vehicle_pattern, element['description']).group(1)
+            status = re.search(status_pattern, element['description']).group(1)
+
+            newobj["Denumire"] =  title
+            newobj["Timp"] =  wait_time
+            newobj["Info"] =  info
+            # element["Latitude"] =  vehicle
+            newobj["Status"] =  element['marker_color']
+            # element["Status"] =  element['status']
+            newobj["Sens"] =  source['sens']
+            newobj["Tip vehicul"] =  source['tip_vehicul']
+            newobj["Denumire"] = element['title']
+            newobj["Latitude"] = element['lat']
+            newobj["Longitude"] = element['lng']
+            combined_data.append(newobj)
+
+         
+    else:
+        print(
+            f'Failed to fetch the URL for source: {source}. Status code: {response.status_code}'
+        )
+        # TODO: logging
+
+# with open(filename + '.csv', 'w', newline='', encoding='utf-8') as csv_file:
+#     writer = csv.writer(csv_file)
+#     writer.writerow(zicolumns)
+#     writer.writerows(combined_data)
+
+csv_obj = pd.DataFrame(combined_data, columns=zicolumns)
+sorted_df = csv_obj.sort_values(by=['Denumire', 'Tip vehicul', 'Sens'])
+
+# csv_obj.to_json(filename + '.json', orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+sorted_df.to_json(filename + '.json',
+                orient="records",
+                force_ascii=False,
+                indent=2)
+sorted_df.to_csv(filename + '.csv', encoding='utf-8', index=False)
+
+print(f'Scraped & saved to {filename}.csv/json')
