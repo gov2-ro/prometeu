8a99bb9b96595a422ff8c6ff0a26c0b2d1e6c87c
pax <pax@mioritics.ro>
2023-09-10 22:46:33 +0300

    deeer headers

--- deer-incidente.py
+++ deer-incidente.py
@@ -25,24 +25,24 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'en-GB,en;q=0.7',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Cookie': 'cookiesession1=56CBB03A3N3S4Z0IZ3SMSQDGTLJM1261',
-    'Origin': 'https://intreruperi.edmn.ro',
-    'Referer': 'https://intreruperi.edmn.ro/intreruperiMN.aspx',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
-    'Sec-GPC': '1',
-    'Upgrade-Insecure-Requests': '1',
+    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    # 'Accept-Language': 'en-GB,en;q=0.7',
+    # 'Cache-Control': 'max-age=0',
+    # 'Connection': 'keep-alive',
+    # 'Content-Type': 'application/x-www-form-urlencoded',
+    # 'Cookie': 'cookiesession1=56CBB03A3N3S4Z0IZ3SMSQDGTLJM1261',
+    # 'Origin': 'https://intreruperi.edmn.ro',
+    # 'Referer': 'https://intreruperi.edmn.ro/intreruperiMN.aspx',
+    # 'Sec-Fetch-Dest': 'document',
+    # 'Sec-Fetch-Mode': 'navigate',
+    # 'Sec-Fetch-Site': 'same-origin',
+    # 'Sec-Fetch-User': '?1',
+    # 'Sec-GPC': '1',
+    # 'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"'
+    # 'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"',
+    # 'sec-ch-ua-mobile': '?0',
+    # 'sec-ch-ua-platform': '"macOS"'
 }
 
 
